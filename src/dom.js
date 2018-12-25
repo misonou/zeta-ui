5e31bc88068438f2fe02ed07318fb8790901a234
Takeru Lee <takeru.moe@gmail.com>
2018-12-25 15:16:13 +0800

    Fix event source for Firefox and composition evt

--- src/dom.js
+++ src/dom.js
@@ -2,7 +2,6 @@
     'use strict';
 
     var KEYNAMES = JSON.parse('{"8":"backspace","9":"tab","13":"enter","16":"shift","17":"ctrl","18":"alt","19":"pause","20":"capsLock","27":"escape","32":"space","33":"pageUp","34":"pageDown","35":"end","36":"home","37":"leftArrow","38":"upArrow","39":"rightArrow","40":"downArrow","45":"insert","46":"delete","48":"0","49":"1","50":"2","51":"3","52":"4","53":"5","54":"6","55":"7","56":"8","57":"9","65":"a","66":"b","67":"c","68":"d","69":"e","70":"f","71":"g","72":"h","73":"i","74":"j","75":"k","76":"l","77":"m","78":"n","79":"o","80":"p","81":"q","82":"r","83":"s","84":"t","85":"u","86":"v","87":"w","88":"x","89":"y","90":"z","91":"leftWindow","92":"rightWindowKey","93":"select","96":"numpad0","97":"numpad1","98":"numpad2","99":"numpad3","100":"numpad4","101":"numpad5","102":"numpad6","103":"numpad7","104":"numpad8","105":"numpad9","106":"multiply","107":"add","109":"subtract","110":"decimalPoint","111":"divide","112":"f1","113":"f2","114":"f3","115":"f4","116":"f5","117":"f6","118":"f7","119":"f8","120":"f9","121":"f10","122":"f11","123":"f12","144":"numLock","145":"scrollLock","186":"semiColon","187":"equalSign","188":"comma","189":"dash","190":"period","191":"forwardSlash","192":"backtick","219":"openBracket","220":"backSlash","221":"closeBracket","222":"singleQuote"}');
-    var EVENT_SOURCES = 'script mouse touch keyboard input drop cut copy paste';
     var SELECTOR_FOCUSABLE = ':input, [contenteditable], a[href], area[href], iframe';
     var META_KEYS = [16, 17, 18, 91, 93];
     var OFFSET_ZERO = {
@@ -55,6 +54,7 @@
     var lastEventSource;
     var eventSource;
     var windowFocusedOut;
+    var currentEvent;
     var scrollbarWidth;
     var _ = helper.createPrivateStore();
 
@@ -80,8 +80,8 @@
     }
 
     function getEventSource() {
-        var type = window.event && window.event.type;
-        return (type && matchWord(type[0] === 'k' ? 'keyboard' : type[0] === 't' ? 'touch' : type[0] === 'm' || matchWord(type, 'wheel click dblclick contextmenu') ? 'mouse' : type, EVENT_SOURCES)) || 'script';
+        var type = (currentEvent || window.event || '').type || '';
+        return type[0] === 'k' || type.substr(0, 3) === 'com' ? 'keyboard' : type[0] === 't' ? 'touch' : type[0] === 'm' || matchWord(type, 'wheel click dblclick contextmenu') ? 'mouse' : matchWord(type, 'drop cut copy paste') || 'script';
     }
 
     function prepEventSource(promise) {
@@ -826,6 +826,9 @@
         prepEventSource: prepEventSource,
         scrollIntoView: scrollIntoView,
         focused: focused,
+        get event() {
+            return currentEvent;
+        },
         get activeElement() {
             return focusPath[0];
         },
@@ -1086,8 +1089,11 @@
             }, true);
         }
 
-        // document.activeElement or FocusEvent.relatedTarget does not report non-focusable element
-        bind(body, 'mousedown mouseup wheel keydown keyup keypress touchstart touchend cut copy paste drop click dblclick contextmenu', function (e) {
+        bind(window, 'mousedown mouseup wheel compositionstart compositionend beforeinput textInput keydown keyup keypress touchstart touchend cut copy paste drop click dblclick contextmenu', function (e) {
+            currentEvent = e;
+            setTimeout(function () {
+                currentEvent = null;
+            });
             lastEventSource = null;
             if (!focusable(e.target)) {
                 e.stopImmediatePropagation();
